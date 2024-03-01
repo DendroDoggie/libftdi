82e363dee5414e9b5fd9b34fa8b9da47a33f2ff5
Zachariah Derrick <72170300+ZaderRox1111@users.noreply.github.com>
2024-03-01 09:31:29 -0700

    Update ftdi.h

--- src/ftdi.h
+++ src/ftdi.h
@@ -518,7 +518,7 @@ extern "C"
 #endif
 
     int ftdi_init(struct ftdi_context *ftdi, int fileDescriptor);
-    struct ftdi_context *ftdi_new(void);
+    struct ftdi_context *ftdi_new(int fileDescriptor);
     int ftdi_set_interface(struct ftdi_context *ftdi, enum ftdi_interface interface);
     int ftdi_set_module_detach_mode(struct ftdi_context *ftdi, enum ftdi_module_detach_mode mode);
 
